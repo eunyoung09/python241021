91b31bee2d44f4702f26bbf47ad7275a965d53bd
eycheon79 <eycheon79@gmail.com>
2024-10-21 16:33:12 +0900

    첫번째 작업

---
+++ if_else.py
@@ -0,0 +1,25 @@
+# score = int(input('점수를 입력: '))
+# if 90 <= score <= 100:
+#     grade = "A"
+# elif 80 <= score < 90:
+#     grade = "B"
+# elif 70 <= score < 80:
+#     grade = "C"
+# elif 60 <= score < 70:
+#     grade = "D"
+# else:
+#     grade = "F"
+    
+# print('등급은 ', grade)
+
+
+#반복구문
+value = 5
+while value > 0:
+    print(value)
+    value -= 2
+
+print("for ~ in 루프")
+lst = ['apple', 100, 3.14]
+for item in lst:
+    print(item)
